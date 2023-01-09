369429199e26296390d98a89bdcf99c3d2d95ed8
erikv <erik.vink@playtech.com>
2023-01-09 19:12:11 +0200

    Initial commit

---
+++ src/main/java/com/example/uusprojekt/UusProjektApplication.java
@@ -0,0 +1,13 @@
+package com.example.uusprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UusProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UusProjektApplication.class, args);
+    }
+
+}
